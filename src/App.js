d1d6fe0cb407ba8afe6dc064645793a742f6d3f5
Eric <ericyoung111@gmail.com>
2022-01-28 16:03:08 -0700

    made the gallery and used state to dynamically update components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-// import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Nav from './components/Nav/index'
 import About from './components/About/index'
+import Gallery from './components/Gallery/index'
 
 function App() {
+  const [categories] = useState([
+    {
+        name: "commercial",
+        description: "Photos of grocery stores, food trucks, and other commercial projects"
+    },
+    { 
+        name: "portraits", 
+        description: "Portraits of people in my life"
+    },
+    {
+        name: "landscape",
+        description: "Fields, farmhouses, waterfalls, and the beauty of nature"
+    }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div>
-      <Nav />
+      <Nav 
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      />
       <main>
+        <Gallery />
         <About />
       </main>
     </div>
