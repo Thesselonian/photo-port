c21c849d04d2904ce1ad86fddd22075704217e6e
Eric <ericyoung111@gmail.com>
2022-01-29 13:42:39 -0700

    making the production build

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './App.css';
+import Nav from './components/Nav/index'
+import About from './components/About/index'
+import Gallery from './components/Gallery/index'
+import ContactForm from './components/Contact/index'
+
+function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+  const [categories] = useState([
+    {
+        name: "commercial",
+        description: "Photos of grocery stores, food trucks, and other commercial projects"
+    },
+    { 
+        name: "portraits", 
+        description: "Portraits of people in my life"
+    },
+    {
+        name: "landscape",
+        description: "Fields, farmhouses, waterfalls, and the beauty of nature"
+    }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  
+  return (
+    <div>
+      <Nav 
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      />
+      <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory} />
+            <About />
+          </>
+        ) : (
+          <ContactForm />
+        )}
+        
+        
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '../index'
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    // First Test
+    it('renders', () => {
+        render(<About />)
+    });
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<about />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
